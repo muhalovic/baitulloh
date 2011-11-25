6662b23c543a2f5c2919b75bf6230c41ddfee972
Nasrul Hadi <nasrul.hadi@live.com>
2011-11-25 15:00:55 +0700

    penambahan data master propinsi

--- application/models/province_model.php
+++ application/models/province_model.php
@@ -4,6 +4,7 @@ class Province_model extends CI_Model {
     function __construct()
     {
         parent::__construct();
+		$this->CI = get_instance();
     }
 	
 	function get_all_province(){
@@ -19,6 +20,59 @@ class Province_model extends CI_Model {
 		$this->db->where('ID_PROPINSI', $id);
 		
 		return $this->db->get();
+	}
+	
+	
+	function get_total_propinsi(){
+		$this->db->select('*');
+		$this->db->from('province');
+		
+		return $this->db->count_all_results();
+	}
+	
+	function get_grid_allover()
+	{
+		$this->db->select('*');
+		$this->db->from('province');
+		
+		$this->CI->flexigrid->build_query();
+		$return['records'] = $this->db->get();
+		
+		$this->db->select('*');
+		$this->db->from('province');
+		
+		$this->CI->flexigrid->build_query(FALSE);
+		$return['record_count'] = $this->db->count_all_results();
+	
+		return $return;
+	}
+	
+	function insert_propinsi($data){
+		$this->db->trans_begin();
+		$this->db->insert('province', $data);
+		
+		if ($this->db->trans_status() === FALSE)
+			$this->db->trans_rollback();
+		else
+			$this->db->trans_commit();
+	}
+	
+	function update_propinsi($data, $id_room){
+		$this->db->trans_begin();
+		$this->db->where('ID_PROPINSI', $id_room);
+		$this->db->update('province', $data);
+		
+		if ($this->db->trans_status() === FALSE)
+			$this->db->trans_rollback();
+		else
+			$this->db->trans_commit();
+	}
+	
+	function hapus_propinsi($id_room)
+	{
+		$this->db->delete('province', array('ID_PROPINSI' => $id_room));
+		
+		return TRUE;
 	}
 }
 
